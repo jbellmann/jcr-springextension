b41e5ff1cf522c536df4db2d7516dadb6966a14c
Joerg Bellmann <joerg.bellmann@googlemail.com>
2011-12-06 22:21:19 +0100

    field marked as final

--- src/main/java/org/springframework/extensions/jcr/jackrabbit/ocm/ResourceBasedDigesterMapperDecorator.java
+++ src/main/java/org/springframework/extensions/jcr/jackrabbit/ocm/ResourceBasedDigesterMapperDecorator.java
@@ -1,21 +1,21 @@
 package org.springframework.extensions.jcr.jackrabbit.ocm;
 
-import org.apache.jackrabbit.ocm.mapper.Mapper;
-import org.apache.jackrabbit.ocm.mapper.impl.digester.DigesterMapperImpl;
-import org.apache.jackrabbit.ocm.mapper.model.ClassDescriptor;
-import org.springframework.core.io.Resource;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.jackrabbit.ocm.mapper.Mapper;
+import org.apache.jackrabbit.ocm.mapper.impl.digester.DigesterMapperImpl;
+import org.apache.jackrabbit.ocm.mapper.model.ClassDescriptor;
+import org.springframework.core.io.Resource;
 
 /**
  * User: dsklyut Date: Dec 8, 2009 Time: 9:33:38 AM
  */
 public class ResourceBasedDigesterMapperDecorator implements Mapper {
 
-    private DigesterMapperImpl decorated;
+    private final DigesterMapperImpl decorated;
 
     public ResourceBasedDigesterMapperDecorator(Resource resource) throws IOException {
         decorated = new DigesterMapperImpl(resource.getInputStream());
@@ -33,10 +33,12 @@ public class ResourceBasedDigesterMapperDecorator implements Mapper {
         return result.toArray(new InputStream[result.size()]);
     }
 
+    @Override
     public ClassDescriptor getClassDescriptorByClass(Class clazz) {
         return this.decorated.getClassDescriptorByClass(clazz);
     }
 
+    @Override
     public ClassDescriptor getClassDescriptorByNodeType(String jcrNodeType) {
         return this.decorated.getClassDescriptorByNodeType(jcrNodeType);
     }
